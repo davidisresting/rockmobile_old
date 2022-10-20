dfde81c854baa009009e0013dfa18f5850876a4f
David Enam <davidjavadavid@gmail.com>
2022-10-20 09:10:53 +0200

    make it better

---
+++ firebase.config.js
@@ -0,0 +1,15 @@
+
+export const credentials = {
+  apiKey: "AIzaSyDTtKPRVfPBuH3F2eeUM4b5uEvlzjYd2yg",
+  authDomain: "rocket-ec86b.firebaseapp.com",
+  databaseURL: "https://rocket-ec86b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rocket-ec86b",
+  storageBucket: "rocket-ec86b.appspot.com",
+  messagingSenderId: "670247851047",
+  appId: "1:670247851047:web:4c289e7d650206dbb26a02",
+  measurementId: "G-S6R4VV8ZKG"
+};
+
+export const config = {
+  name: 'SECONDARY_APP',
+};
