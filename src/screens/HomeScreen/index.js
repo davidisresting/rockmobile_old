7461a936bd86b7d69a773cf6757cb4d250f0e00b
David Enam <davidjavadavid@gmail.com>
2022-10-20 12:33:36 +0200

    Revert "make it better"
    This reverts commit dfde81c854baa009009e0013dfa18f5850876a4f.

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,30 +1,10 @@
 import React from 'react'
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import auth from '@react-native-firebase/auth';
-import styles from './styles';
-
+import { Text, View } from 'react-native'
 
 export default function HomeScreen(props) {
-
-    console.log('HomeScreenprops', props);
-
-    const onLogoutPress = () => {
-        auth()
-        .signOut()
-        .then(() => console.log('User signed out!'));
-        props.navigation.navigate('Login')
-    }
-
     return (
-        <View style={styles.container}>
-            <View style={styles.footerView}>
-                <Text style={styles.footerText}>Hi {props.extraData?.fullName}!</Text>
-            </View>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={onLogoutPress}>
-                <Text style={styles.buttonTitle}>Sign out</Text>
-            </TouchableOpacity>                
+        <View>
+            <Text>Home Screen</Text>
         </View>
     )
 }
